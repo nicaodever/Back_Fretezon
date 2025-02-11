04b347822f74ed1b556d807dcbe46a7d7831dbd3
Cassio <cassio.tavares4@gmail.com>
2025-02-10 21:15:37 -0400

    atualização do models

--- backft/core/models.py
+++ backft/core/models.py
@@ -44,5 +44,119 @@ class Frete(models.Model):
         null=False,
         max_length=255
     )
+    destino = models.CharField(
+        db_column='destino',
+        null=False,
+        max_length=255
+    )
+    horario_retir = models.DateField(
+        db_column='horario_retir',
+        null=False
+    )
+    sugerir_tarifa = models.FloatField(
+        db_column='sugerir_tarifa',
+        null=False
+    )
+    foto_carga = models.DateField(
+        db_column='foto_carga',
+        null=False
+    )
+    data_entrega = models.DateField(
+        db_column='data_entrega',
+        null=False
+    )
+    descricao_carga = models.CharField(
+        db_column='descricao_carga',
+        max_length=150,
+        null=False
+    )
 
+class Motorista(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        unique=True,
+        db_column='id'
+    )
+    name = models.CharField(
+        db_column='name',
+        max_length=250,
+        null=False
+    )
+    data_nascimento = models.DateField(
+        db_column='data_nascimento',
+        null=False
+    )
+    cpf = models.IntegerField(
+        db_column='cpf',
+        null=False
+    )
+    Telefone = models.IntegerField(
+        db_column='Telefone',
+        null=False
+    )
+    email = models.CharField(
+        db_column='email',
+        max_length=250,
+        null=False
+    )
+    cidade = models.CharField(
+        db_column='cidade',
+        max_length=250,
+        null=False
+    )
+    estado = models.CharField(
+        db_column='estado',
+        max_length=250,
+        null=False
+    )
 
+class Veiculo(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        unique=True,
+        db_column='id'
+    )
+    modelo = models.CharField(
+        db_column='Modelo',
+        max_length=50,
+        null=False
+    )
+    placa = models.CharField(
+        db_column='placa',
+        max_length=6,
+        null=False
+    )
+    marca = models.CharField(
+        db_column='marca',
+        max_length=30,
+        null=False
+    )
+    ano = models.IntegerField(
+        db_column='ano',
+        null=False
+    )
+
+class CNN(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+        unique=True,
+        db_column='id'
+    )
+    status_cnh = models.CharField(
+        db_column='status_cnh',
+        max_length=250,
+        null=False
+    )
+    data_emicao_cnh = models.DateField(
+        db_column='data_emicao_cnh',
+        null=False
+    )
+    categoryta_cnh = models.CharField(
+        db_column='categoryta_cnh',
+        max_length=250,
+        null=False
+    )
+    validate_cnh = models.DateField(
+        db_column='validate_cnh',
+        null=False
+    )
